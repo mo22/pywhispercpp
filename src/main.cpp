f894a0bad2bfc161e2285d94902daf8b3301c8f2
Moritz Moeller <mm@mxs.de>
2025-05-09 14:03:55 +0200

    ..

--- src/main.cpp
+++ src/main.cpp
@@ -56,9 +56,11 @@ struct whisper_model_loader_wrapper {
 };
 
 struct whisper_context_wrapper whisper_init_from_file_wrapper(const char * path_model){
+    fprintf(stderr, "whisper_init_from_file_wrapper [%s]\n", path_model);
+
     struct whisper_context_params params = whisper_context_default_params();
-    // params.dtw_token_timestamps = true;
-    // params.dtw_aheads_preset = WHISPER_AHEADS_LARGE_V2;
+    params.dtw_token_timestamps = true;
+    params.dtw_aheads_preset = WHISPER_AHEADS_LARGE_V2;
     struct whisper_context * ctx = whisper_init_from_file_with_params(path_model, params);
     struct whisper_context_wrapper ctw_w;
     ctw_w.ptr = ctx;
